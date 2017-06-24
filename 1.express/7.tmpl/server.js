443dd5c6afff243f91fbde5a829fc2fbf1281cd9
zhufengnodejs <zhufengnodejs@126.com>
2017-06-24 17:48:28 +0800

    next

--- 1.express/7.tmpl/server.js
+++ 1.express/7.tmpl/server.js
@@ -1,6 +1,14 @@
 let express = require('express');
 let path = require('path');
+let bodyParser = require('body-parser');
+let querystring = require('querystring');
 let app = express();
+//每个中间件都 会处理自己类型的数据，通过请求头中的content-type字段来判断
+//使用此中间件之后，会在req.body
+//Content-Type:application/json
+app.use(bodyParser.json());
+//Content-Type:application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
 //要在express中使用模板引擎
 //1.设置模板引擎 当查找文件的时候会添加此后缀名去找文件
 app.set('view engine','html');
@@ -23,6 +31,25 @@ app.get('/signup',function(req,res){
     //render第1参数是模板的相对路径,是相对于views目录的相对路径
     //第2参数是数据对象
     res.render('signup',{title:'用户注册'});
+});
+/*app.post('/signup',function(req,res){
+  let bufs = [];
+  req.on('data',function(data){
+      bufs.push(data);
+  });
+  req.on('end',function(){
+      let result = Buffer.concat(bufs).toString();
+      //username=1&password=2
+      console.log(result);
+      let user = querystring.parse(result);
+      console.log(user);
+      users.push(user);
+      res.send(users);
+  });
+});*/
+app.post('/signup',function(req,res){
+    users.push(req.body);
+    res.send(users);
 });
 app.get('/users',function(req,res){
     res.render('users',{title:'用户列表',users});
