0a98c893a9c8d5e8bc69989fe46b8bf141bb0182
zhufengnodejs <zhufengnodejs@126.com>
2017-06-24 12:47:00 +0800

    next

--- 1.express/next.js
+++ 1.express/next.js
@@ -1,19 +1,19 @@
-let arr = [];
+let req = {};
 let fn1 = function(next){
     console.log(1);
+    req.money = 100;;
     next();
 }
 let fn2 = function(next){
-    console.log(2);
+    console.log(2,req.money);
     next();
 }
 let fn3 = function(next){
     console.log(3);
 }
-arr.push(fn1);
-arr.push(fn2);
-arr.push(fn3);
+let arr = [fn1,fn2,fn3];
 let index = 0;
+
 let next = ()=>{
     let fn = arr[index++];
     fn(next);
