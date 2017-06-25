890e075832b46f81943e299172fe1044b6981ef2
zhufengnodejs <zhufengnodejs@126.com>
2017-06-25 17:45:21 +0800

    path

--- 3.session/1.session.js
+++ 3.session/1.session.js
@@ -7,24 +7,32 @@ let cookieParser = require('cookie-parser');
 let app = express();
 app.use(cookieParser());
 //这是服务器向客户端写入卡号时候的key(属性名)
-let SESSION_KEY = 'connect.sid';
+let SESSION_KEY = 'xxx';
 //在服务器端开辟内存，存放每个卡号对应的信息
 let sessions = {};
 app.get('/visit',function(req,res){
     //从客户端发过来的cookie中得到卡号
-    let sessionId = req.cookies[SESSION_KEY];// 899
+    let sessionId = req.cookies['xyz'];// 899
     if(sessionId){
         //通过卡号找到此卡号在服务器端记录
         let sessionObj = sessions[sessionId];
-        sessionObj.amount -= 100;
-        res.send(`你好，欢迎你老顾客 ，你的卡还有${sessionObj.amount}元`);
+        if(sessionObj){
+            sessionObj.amount -= 100;
+            res.send(`你好，欢迎你老顾客 ，你的卡还有${sessionObj.amount}元`);
+        }else{
+            newCard();
+        }
+
     }else{//如果客户端是第一次访问服务器
+        newCard();
+    }
+    function newCard(){
         //先生成一个卡号,卡号唯一，不容易我被猜到 899
         let newSessionId = Date.now()+'_'+Math.random();
         //在服务器端记录此卡号对应的余额 899
         sessions[newSessionId] = {amount:1000};
         //把生成的卡号通过cookie发送给客户端
-        res.cookie(SESSION_KEY,newSessionId);
+        res.cookie('xyz',newSessionId);
         res.send(`客人，欢迎你初次光临，送你一张1000元的卡`);
     }
 });
---
+++ 3.session/2.session.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let session = require('express-session');
+let app = express();
+app.use(session({
+    resave:true,//每次客户端请求过来的时候都要重新保存session
+    saveUninitialized:true,//保存未使用过的session
+    secret:'zfpx' //是用来加密cookie的
+}));
+app.get('/write',function(req,res){
+  req.session.amount = 1000;
+  res.send('ok');
+});
+//s%3AKUCHWY8UHlToB1NvnbakQlrPuRXYnrCh.MbyAwMU2VoJda5lk%2Bz2TIP84D02Ov4QgdufERQQ6XRg
+app.get('/read',function(req,res){
+    res.send(req.session.amount+'元');
+});
+
+app.get('/visit',function(req,res){
+
+});
+app.listen(8080);
