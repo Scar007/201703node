3a4f221a196643d609d84577fc41ba6a5d1faa29
zhufengnodejs <zhufengnodejs@126.com>
2017-06-27 09:54:42 +0800

    class

---
+++ 4.react/1.mock/1.js
@@ -0,0 +1,12 @@
+class Button{
+  //渲染 表示这个组件长成什么样子
+  render(){
+     return (
+         `
+          <button class="my-btn">
+             <span class="my-text">点赞</span>
+          </button>
+         `
+     )
+  }
+}
