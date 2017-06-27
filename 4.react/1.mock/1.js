f112f4af526d213bad3ee869ccb8e8c661b7f328
zhufengnodejs <zhufengnodejs@126.com>
2017-06-27 10:22:34 +0800

    class

--- 4.react/1.mock/1.js
+++ 4.react/1.mock/1.js
@@ -1,12 +1,24 @@
 class Button{
   //渲染 表示这个组件长成什么样子
+  createDOMFromString(str){
+    let div = document.createElement('div');
+    div.innerHTML = str;
+    return div.children[0];
+  }
   render(){
-     return (
-         `
+     let element = this.createDOMFromString(`
           <button class="my-btn">
              <span style="color:blue" class="my-text">点赞</span>
           </button>
-         `
-     )
+         `);
+     element.addEventListener('click',function(){
+         let myText = document.querySelector('.my-text');
+         if(myText.innerText == '点赞'){
+             myText.innerText = '取消';
+         }else{
+             myText.innerText = '点赞';
+         }
+     })
+     return element;
   }
 }
