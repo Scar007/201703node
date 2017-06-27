482ac5a5c025ccfc8055243cb2d2ea4b9762719c
zhufengnodejs <zhufengnodejs@126.com>
2017-06-27 10:51:00 +0800

    class

--- 4.react/1.mock/1.js
+++ 4.react/1.mock/1.js
@@ -1,24 +1,33 @@
 class Button{
+    constructor(){
+        //状态指的是一个组件内部的变量
+        this.state = {star:false};
+    }
   //渲染 表示这个组件长成什么样子
   createDOMFromString(str){
     let div = document.createElement('div');
     div.innerHTML = str;
     return div.children[0];
   }
-  render(){
-     let element = this.createDOMFromString(`
+  handleClick(){
+    this.state = {star:!this.state.star};
+    //先缓存老的button对象
+    let oldEle = this.element;
+    //根据新的state生成新的element对象
+    this.createElement();
+    //把父元素中老button用新的button对象替换掉
+    oldEle.parentElement.replaceChild(this.element,oldEle);
+  }
+  createElement(){
+      this.element = this.createDOMFromString(`
           <button class="my-btn">
-             <span style="color:blue" class="my-text">点赞</span>
+             <span style="color:blue" class="my-text">${this.state.star?'取消':'点赞'}</span>
           </button>
          `);
-     element.addEventListener('click',function(){
-         let myText = document.querySelector('.my-text');
-         if(myText.innerText == '点赞'){
-             myText.innerText = '取消';
-         }else{
-             myText.innerText = '点赞';
-         }
-     })
-     return element;
+      this.element.addEventListener('click',this.handleClick.bind(this))
+  }
+  render(){
+      this.createElement();
+     return this.element;
   }
 }
