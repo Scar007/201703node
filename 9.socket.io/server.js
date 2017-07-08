4e7b5b28057a7cb752bfdd22cec55b2de70668c8
zhufengnodejs <zhufengnodejs@126.com>
2017-07-08 12:28:29 +0800

    绘制面板

--- 9.socket.io/server.js
+++ 9.socket.io/server.js
@@ -4,12 +4,19 @@
  * 消息通信 socket.io
  */
 let express = require('express');
+let path = require('path');
 let app = express();
+//path must be absolute or specify root to res.sendFile
 app.get('/',function(req,res){
-   res.sendFile('./index.html');
+   res.sendFile(path.resolve('./index.html'));
 });
 //然后会得到一个服务器的实例
 let server = require('http').createServer(app);
 
-
 server.listen(8080);
+/**
+ * 1.实现匿名聊天
+ *    1. 一个人发言后会发给服务器
+ *    2. 服务器会广播给所有的客户端
+ *    3. 所有的客户端收到消息后会在列表里显示出这条消息
+ **/
